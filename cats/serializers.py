fa689c7fb670560338c69876ccfa50c3c70fe096
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2025-03-10 14:49:28 +0500

    ok 10032025 1449

--- cats/serializers.py
+++ cats/serializers.py
@@ -14,13 +14,21 @@ class CatSerializer(serializers.ModelSerializer):
     # Убрали owner = serializers.StringRelatedField(read_only=True)
     # owner = serializers.StringRelatedField(read_only=True)
     # Переопределяем поле achievements
-    achievements = AchievementSerializer(many=True)  # Убрали read_only=True
+    achievements = AchievementSerializer(many=True, required=False)  # Убрали read_only=True
 
     class Meta:
         model = Cat
         fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
 
     def create(self, validated_data):
+        
+        # Если в исходном запросе не было поля achievements
+        if 'achievements' not in self.initial_data:
+            # То создаём запись о котике без его достижений
+            cat = Cat.objects.create(**validated_data)
+            return cat
+        # Иначе делаем следующее:
+
         # Уберём список достижений из словаря validated_data и сохраним его
         achievements = validated_data.pop('achievements')
 
@@ -30,12 +38,10 @@ class CatSerializer(serializers.ModelSerializer):
         # Для каждого достижения из списка достижений
         for achievement in achievements:
             # Создадим новую запись или получим существующий экземпляр из БД
-            current_achievement, status = Achievement.objects.get_or_create(
-            **achievement)
+            current_achievement, status = Achievement.objects.get_or_create(**achievement)
             # Поместим ссылку на каждое достижение во вспомогательную таблицу
             # Не забыв указать к какому котику оно относится
-            AchievementCat.objects.create(
-                achievement=current_achievement, cat=cat)
+            AchievementCat.objects.create(achievement=current_achievement, cat=cat)
         return cat
 
 
