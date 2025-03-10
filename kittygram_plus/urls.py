1117272cf9c5de1d13f73fe5a5cef512b6a091d9
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2025-03-10 14:22:27 +0500

    ok 10023025 1422

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -2,11 +2,13 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet
+from cats.views import CatViewSet, OwnerViewSet, AchievementSerializer
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
+router.register('Achievement', AchievementSerializer)
 
 urlpatterns = [
     path('', include(router.urls)),
